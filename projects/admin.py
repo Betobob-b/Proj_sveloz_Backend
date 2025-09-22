9c382b88d8b66ae09b4612e4c45c2cfd60dc1bc4
roberto <joserbp20849@gmail.com>
2025-09-22 20:50:27 -0300

    Implement Project and Task models with admin registration and user assignment

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+#admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('creator',)
+
+    def save_model(self, request, obj, form, change):
+
+        if not obj.pk:
+
+            obj.creator = request.user
+
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Project, ProjectAdmin)
--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+#admin.site.register(Task)
+
+class TaskAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('responsible',)
+
+    def save_model(self, request, obj, form, change):
+
+        if not obj.pk:
+            obj.responsible = request.user
+
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Task, TaskAdmin)
